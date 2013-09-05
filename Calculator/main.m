a39a166a632eba4e37e10e60cc3a531e0a23c88f
Ovi Bortas <ovibortas@Ovis-MacBook-Pro.local>
2013-09-05 18:58:35 -0400

    Initial Commit

---
+++ Calculator/main.m
@@ -0,0 +1,22 @@
+//
+//  main.m
+//  Calculator
+//
+//  Created by Ovi Bortas on 9/5/13.
+//  Copyright (c) 2013 Ovi Bortas. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+int main(int argc, const char * argv[])
+{
+
+    @autoreleasepool {
+        
+        // insert code here...
+        NSLog(@"Hello, World!");
+        
+    }
+    return 0;
+}
+
